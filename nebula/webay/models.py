8fa3936e9a18b1c8354594b30d69222ef55fc60c
aliyahb96 <aliyah96b@gmail.com>
2019-11-18 22:15:39 +0000

    cleaned up migrations

--- nebula/webay/models.py
+++ nebula/webay/models.py
@@ -8,7 +8,7 @@ from django.core.validators import RegexValidator
 class Item(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
-    item_pic = models.ImageField(upload_to='webay/media/item_pics')
+    item_pic = models.ImageField(upload_to='item_pics')
     base_price = models.DecimalField(max_digits=5, decimal_places=2)
     start_datetime = models.DateTimeField('%d/%m/%Y %H:%M:%S')
     end_datetime = models.DateTimeField('%d/%m/%Y %H:%M:%S')
@@ -26,7 +26,7 @@ class Bid(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     dob = models.DateField()
-    profile_pic = models.ImageField(default='default.jpg', upload_to='webay/media/profile_pics')
+    profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
     address = models.CharField(max_length=255)
     mobile_regex = RegexValidator(regex=r'^0\d{10}$', message='Mobile number must be a valid 11 digit UK number.')
     mobile = models.CharField(validators=[mobile_regex], max_length=11)
