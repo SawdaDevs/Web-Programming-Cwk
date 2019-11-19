88e77cc9b90ab2f0d0fa36aeff38a5eef9d5e4c7
aliyahb96 <aliyah96b@gmail.com>
2019-11-19 16:16:00 +0000

    merged master into notification-system

--- nebula/webay/static/js/script.js
+++ nebula/webay/static/js/script.js
@@ -2,3 +2,41 @@ $(document).ready(() => {
    $('#id_dob').attr('type', 'date');
     console.log('loaded!!')
 });
+
+function progressHandler(event) {
+   var percent = (event.loaded / event.total) * 100;
+   $('#progressBar').val(Math.round(percent));
+}
+
+function completeHandler(event) {
+   $('#progressBar').val(0);
+   $('#progressBar').hide();
+}
+
+//UPLOADING IMAGE USING AJAX.
+$(function () {
+$('#img_file').change(function uploadFile() {
+	console.log('yooo')
+   $('#progressBar').show();
+   var formdata = new FormData();
+   var file = document.getElementById('img_file').files[0];
+   formdata.append('img_file', file);
+   formdata.append('csrfmiddlewaretoken', $('input[name=csrfmiddlewaretoken]').val());
+   $.ajax({
+      xhr: function () {
+         var xhr = new window.XMLHttpRequest();
+         xhr.upload.addEventListener('progress', progressHandler, false);
+         xhr.addEventListener('load', completeHandler, false);
+         return xhr;
+      },
+      type : 'POST',
+      url  : '/uploadimage/',
+      data : formdata,
+      success: function(data) {
+         $('#profile-img').attr("src",data);
+      },
+      processData : false,
+      contentType : false,
+   });
+});
+});
--- nebula/webay/urls.py
+++ nebula/webay/urls.py
@@ -7,7 +7,8 @@ app_name='webay'
 urlpatterns = [
     path('', views.index, name='index'),
     path("register/", views.register, name="register"),
-    path("profile/", views.profile, name="profile"),
+    path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='webay/login.html'), name='login'),
     path('logout/', views.logout, name="logout"),
+    path('uploadimage/', views.upload_image, name='uploadimage'),
 ]
