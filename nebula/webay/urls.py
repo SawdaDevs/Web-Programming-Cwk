29da855b8887e961e7289f69d3f21496bb588302
aliyahb96 <aliyah96b@gmail.com>
2019-11-22 17:29:39 +0000

    sarah's stuff

--- nebula/webay/urls.py
+++ nebula/webay/urls.py
@@ -2,14 +2,17 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
+from .views import ItemDetailView, ItemDeleteView
+
 app_name='webay'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path("register/", views.register, name="register"),
+    path('index', views.index, name='index'),
+    path('', views.auctions, name='auctions'),
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+    path('item/<int:pk>/delete/', ItemDeleteView.as_view(), name='item-delete'),
+    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='webay/login.html'), name='login'),
-    path('logout/', views.logout, name="logout"),
-    path('uploadimage/', views.upload_image, name='uploadimage'),
-    path('additems/', views.add_item, name='additem'),
+    path('logout/', views.logout, name='logout'),
 ]
--- nebula/webay/views.py
+++ nebula/webay/views.py
@@ -1,23 +1,49 @@
 from django.contrib.auth import logout as django_logout
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.utils import timezone
-from webay.forms import UserForm, UserProfileForm, ProfileImageForm, ItemForm, ItemImageForm
-from webay.models import UserProfile
+from webay.forms import UserForm, UserProfileForm, ProfileImageForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic import TemplateView
 
-# Create your views here.
-
-def not_logged_in(user):
-    return not user.is_authenticated
-
+from .models import Item, Bid
 
 def index(request):
     return render(request, 'webay/base.html')
 
+def auctions(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'webay/auctions.html', context)
 
-@user_passes_test(not_logged_in, login_url='/profile')
+def bids(request):
+    context = {
+        'bids': Bid.objects.filter()
+    }
+    return render(request, 'webay/bids.html', context)
+
+class ItemDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Item
+    success_url = '/'
+
+    def test_func(self):
+        item = self.get_object()
+        if self.request.user == item.user:
+            return True
+        return False
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'webay/item_detail.html'
+    query_pk_and_slug = True
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemDetailView, self).get_context_data(**kwargs)
+        context['bids'] = Bid.objects.all()
+        return context
+
 def register(request):
     registered = False
     if request.method == 'POST':
@@ -48,75 +74,9 @@ def register(request):
                    })
 
 
-@login_required
 def profile(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        mobile = request.POST['mobile']
-        email = request.POST['email']
-        address = request.POST['address']
-
-        profile = UserProfile.objects.get(user=request.user)
-        user = User.objects.get(id=request.user.id)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.email = email
-        profile.mobile = mobile
-        profile.address = address
-
-        user.save()
-        profile.save()
-
-        return redirect('webay:profile')
-
-
-    else:
-        profile = UserProfile.objects.get(user=request.user)
-    return render(request, 'webay/profile_form.html', {'profile': profile})
-
-
-@login_required
-def add_item(request):
-    user = User.objects.get(pk=request.user.pk)
-    if request.method == 'POST':
-        item_form = ItemForm(data=request.POST)
-        item_image_form = ItemImageForm(data=request.POST)
-        if item_form.is_valid():
-            item = item_form.save(commit=False)
-            item.user = user
-            item.start_datetime = timezone.now()
-            if 'item_pic' in request.FILES:
-                item.item_pic = request.FILES['item_pic']
-            item.save()
-            return redirect('webay:profile')  # Modify redirect so it goes to my items once you've finished that section
-
-        else:
-            print(item_form.errors, item_image_form.errors)
-    else:
-        item_form = ItemForm(initial={'user': user.pk})
-        item_image_form = ItemImageForm()
-    return render(request, 'webay/additem.html',
-                  {
-                      'itemForm': item_form,
-                      'itemImageForm': item_image_form,
-                  })
-
-
-@login_required
-def upload_image(request):
-    if 'img_file' in request.FILES:
-        image_file = request.FILES['img_file']
-        profile = UserProfile.objects.get(user=request.user)
-
-        if profile:
-            # if user doesn't have a profile yet
-            # need to create a profile first
-            profile.profile_pic = image_file
-            profile.save()
-        return HttpResponse(profile.profile_pic.url)
-    else:
-        raise Http404('Image file not received')
+    print(request.user.username)
+    return render(request, 'webay/profile.html')
 
 
 @login_required
